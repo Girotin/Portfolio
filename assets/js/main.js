1d2bbd17f63a97fddad51a5b172abe63c86dd147
Gabriel Augusto Giroto <45373992+Girotin@users.noreply.github.com>
2024-12-01 15:06:04 -0300

    Update all the files.

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,106 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
+sr.reveal('.home__data, .about__img, .about__img2, .about__img3, .textoContato, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
 sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
+
+/* Modo Escuro */
+const themeToggle = document.getElementById('theme-toggle');
+themeToggle.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+});
+
+const toggleDarkMode = () => {
+    // Toggle o tema
+    document.body.classList.toggle("dark-mode");
+    
+    // Salvar no localStorage a preferência do tema
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
+  };
+
+  // Carregar a preferência de tema quando a página carregar
+window.addEventListener("DOMContentLoaded", () => {
+    const savedTheme = localStorage.getItem("theme");
+    
+    if (savedTheme) {
+      document.body.classList.add(savedTheme); // Adiciona o tema salvo (dark ou light)
+    }
+  });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("theme-toggle");
+
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode');
+      }
+
+    button.addEventListener("click", function () {
+        // Alternar o tema
+        const body = document.body;
+        body.classList.toggle("dark-mode"); // Troca o tema
+        document.body.classList.toggle('dark-mode');
+        const isDarkMode = body.classList.contains("dark-mode");
+
+        // Alterar a logo com base no tema
+        const logo = document.getElementById("logo");
+        if (logo) {
+            if ((isDarkMode) == false) {
+                logo.outerHTML = `
+                    <svg id="logo" class="nav__logo" viewBox="0 0 100 22">
+                        <defs>
+                            <linearGradient id="gradient-dark">
+                                <stop offset="0%" stop-color="#000" />
+                                <stop offset="100%" stop-color="#444" />
+                            </linearGradient>
+                            <pattern id="wave-dark" x="0" y="1" width="100%" height="100%" patternUnits="userSpaceOnUse">
+                                <path id="wavePath-dark" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" 
+                                      mask="url(#mask-dark)" fill="url(#gradient-dark)">
+                                    <animateTransform
+                                        attributeName="transform"
+                                        begin="0s"
+                                        dur="1.5s"
+                                        type="translate"
+                                        from="0,0"
+                                        to="40,0"
+                                        repeatCount="indefinite" />
+                                </path>
+                            </pattern>
+                        </defs>
+                        <text text-anchor="middle" x="13" y="15" font-size="10" fill="white" fill-opacity="0.1">Giroto</text>
+                        <text text-anchor="middle" x="13" y="15" font-size="10" fill="url(#wave-dark)" fill-opacity="1">Giroto</text>
+                    </svg>`;
+            } else {
+                logo.outerHTML = `
+                    <svg id="logo" class="nav__logo" viewBox="0 0 100 22">
+                        <defs>
+                            <linearGradient id="gradient-light">
+                                <stop offset="0%" stop-color="#fff" />
+                                <stop offset="100%" stop-color="#ccc" />
+                            </linearGradient>
+                            <pattern id="wave-light" x="0" y="1" width="100%" height="100%" patternUnits="userSpaceOnUse">
+                                <path id="wavePath-light" d="M-40 9 Q-30 7 -20 9 T0 9 T20 9 T40 9 T60 9 T80 9 T100 9 T120 9 V20 H-40z" 
+                                      mask="url(#mask-light)" fill="url(#gradient-light)">
+                                    <animateTransform
+                                        attributeName="transform"
+                                        begin="0s"
+                                        dur="1.5s"
+                                        type="translate"
+                                        from="0,0"
+                                        to="40,0"
+                                        repeatCount="indefinite" />
+                                </path>
+                            </pattern>
+                        </defs>
+                        <text text-anchor="middle" x="13" y="15" font-size="10" fill="black" fill-opacity="0.1">Giroto</text>
+                        <text text-anchor="middle" x="13" y="15" font-size="10" fill="url(#wave-light)" fill-opacity="1">Giroto</text>
+                    </svg>`;
+            }
+        }
+    });
+});
